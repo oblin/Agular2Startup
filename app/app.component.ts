ce6cda54855b38e20c6e075c0daea2f24bd60219
yiming lin <yiming.lin@outlook.com>
2016-10-15 21:21:37 +0800

    Startup with initialized

---
+++ app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-app',
+    template: "Starter files for Angular2: Getting Started."
+    // template: `
+    // <div>
+    //     <nav class='navbar navbar-default'>
+    //         <div class='container-fluid'>
+    //             <a class='navbar-brand'>{{pageTitle}}</a>
+    //             <ul class='nav navbar-nav'>
+    //                 <li><a [routerLink]="['/welcome']">Home</a></li>
+    //                 <li><a [routerLink]="['/products']">Product List</a></li>
+    //             </ul>
+    //         </div>
+    //     </nav>
+    //     <div class='container'>
+    //         <router-outlet></router-outlet>
+    //     </div>
+    //  </div>
+    //  `
+})
+export class AppComponent {
+    pageTitle: string = 'Acme Product Management';
+}
